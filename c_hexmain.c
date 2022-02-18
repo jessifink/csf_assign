65f660606f70eea3f57db38fca48fdda6d889aed
Jessi Finkelstein <jfinke14@jh.edu>
2022-02-18 18:29:00 -0500

    not printing first letter but working

--- c_hexmain.c
+++ c_hexmain.c
@@ -4,24 +4,23 @@
 #include "stdio.h"
 
 int main(void) { 
-  printf("%s \n", "hello");
   char input[100]; 
+  char output[100];
   int count = 0;
   int length = hex_read(input);
-    printf("%d \n", length);
-
   while (length > 0) {
-    hex_format_offset(count,input);
-    hex_write_string(input);
+    hex_format_offset(count,output);
+    hex_write_string(output);
     write(1,": ", 1); 
-    for (int i = 0; i < length; i++ ) {
+    for (int i = 0; i < length;  i++ ) {
+      printf("%c", input[i]);
       //hex_to_printable(input[i]);
-      hex_format_byte_as_hex(input[i],input);
-      hex_write_string(input);
+      hex_format_byte_as_hex(input[i],output);
+      hex_write_string(output);
       write(1,"  ", 1); 
     }
     count += 16;
-    hex_write_string(input);
+    hex_write_string(output);
     write(1,"\n", 1); 
     length = hex_read(input);
   }

e16e4501b646c9a8234ce1f3586d0f521ad956e3
nbalaji2 <nbalaji2@jh.edu>
2022-02-22 10:40:03 -0500

    formatting

--- c_hexmain.c
+++ c_hexmain.c
@@ -8,21 +8,30 @@ int main(void) {
   char output[100];
   int count = 0;
   int length = hex_read(input);
-  while (length > 0) {
+  while (length <= 16 && length > 1) {
     hex_format_offset(count,output);
     hex_write_string(output);
-    write(1,": ", 1); 
-    for (int i = 0; i < length;  i++ ) {
-      printf("%c", input[i]);
-      //hex_to_printable(input[i]);
+    hex_write_string(": ");
+    //write(1,": ", 2); 
+    for (int i = 0; i < length; i++ ) {
+      //write(1, input[i], 4);
+      //printf("%c", input[i]);      
       hex_format_byte_as_hex(input[i],output);
       hex_write_string(output);
-      write(1,"  ", 1); 
+      hex_write_string(" ");
+    }
+    
+    int l = length;
+    while (l < 16) {
+      write(1, "    ", 3);
+      l++;  
     }
     count += 16;
-    hex_write_string(output);
-    write(1,"\n", 1); 
+    hex_write_string(input); 
+    //issue is in this line somewhere on last run
     length = hex_read(input);
+    write(1,"\n", 1);
   }
+  
   return 0;
 }

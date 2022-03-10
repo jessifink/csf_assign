35b238b645e9d492957be7c03ec7af841a3c0d24
nbalaji2 <nbalaji2@jh.edu>
2022-03-10 11:29:53 -0500

    command line args

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,3 +1,47 @@
-//hello
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
+    int sets = 0;
+    int blocks = 0;
+    int bytes = 0;
+    bool wa = false;
+    bool wt = false;
+    bool isLru = false;
+    string file_name = "";
+
+
+    if (argc != 8) { //check for valid number of arguments
+        return 1;
+    }
+
+    sets = (int) argv[1];
+    blocks = (int) argv[2];
+    bytes = (int) argv[3];
+
+    string input = argv[4];
+    string cmp = "write-allocate";
+    if (input == cmp) {
+        wa == true;
+    }
+
+    input = argv[5];
+    string cmp = "write-through";
+    if (input == cmp) {
+        wt == true;
+    }
+
+    input = argv[6];
+    string cmp = "lru";
+    if (input == cmp) {
+        isLru = true;
+    }
+
+    input = argv[7];
+    file_name = input;
+
+    return 0;
 }

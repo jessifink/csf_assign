db1ec84a8495468583029c587c94abe00aef4157
Jessi Finkelstein <jfinke14@jh.edu>
2022-04-10 16:17:10 -0400

    finished?

--- csf_assign04_cplusplus/magic.cpp
+++ csf_assign04_cplusplus/magic.cpp
@@ -17,25 +17,38 @@
 int main(int argc, char **argv) {
 //open the specified file as command-line argument
   if (argc != 2) {
-    std::cout << "Error: invalid number of arguments\n";
+    std::cerr << "Error: invalid number of arguments\n";
     return 1;
   }
-  
+
+
   char *  filename = argv[1];
 
   //use mmap to map data to memory
   int fd = open(filename, O_RDONLY);
+  if (fd < 0) {  // If cannot open file
+    std::cerr << "ERROR: Cannot open file" << std::endl;
+    return 2;  
+  }
   struct stat statbuf;
   int rc = fstat(fd, &statbuf);
   size_t file_size;
   if (rc != 0) {
-    // error
+    std::cerr << "Error: fstat failed\n";
+    return 2;
   } else {
     file_size = statbuf.st_size;
-    // ...
   }
+
   void *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
   Elf64_Ehdr *elf_base = (Elf64_Ehdr *) data;
+    if (elf_base->e_ident[0] != 127 ||
+      elf_base->e_ident[1] != 69 ||
+      elf_base->e_ident[2] != 76 ||
+      elf_base->e_ident[3] != 70) {  
+    std::cout << "Not an ELF file" << std::endl;
+    return 0;
+  }
   
   //print header summary
   std::cout << "Object file type: ";
@@ -74,9 +87,29 @@ int main(int argc, char **argv) {
    std::cout << ", size=";
    printf("%lx", section_header[i].sh_size);
    std::cout << "\n";
-   
-   
+   std::string name = (char*) (data + sh_string_table->sh_offset + section_header[i].sh_name);
+   std::string symtab = ".symtab";
+   if (symtab.compare(name) == 0) {
+    int sym_counter = section_header->sh_size / section_header->sh_entsize;
+    Elf64_Sym* sym_table = (Elf64_Sym*) ((unsigned char*) data + section_header->sh_offset); 
+    for (int j = 0; j < sym_counter; j++) {
+      std::cout << "Symbol " << i; 
+      if (sym_table->st_name != 0) {
+        std::cout << ": name=";
+        std::cout <<  (char*) (data + sym_table->st_name + section_header[i].sh_offset);
+      } else {
+         std::cout << ": name= ";
+      }
+      std::cout << ", size=";
+      printf("%lx", sym_table->st_size);
+      std::cout << ", info=";
+      printf("%lx", sym_table->st_info);
+      std::cout << ", other=";
+      printf("%lx", sym_table->st_other);
+    }
   }
+  }
+
   
   //determine whether file is ELF file or not and print output
   //if ELF file, summarize and exit
